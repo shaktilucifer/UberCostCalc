8625f64a96c8bc2286d3d9eee00e344d404fd714
shakti lucifer <shakticse@gmail.com>
2017-04-08 22:54:58 -0400

    Add controller and requests node moduel

---
+++ uberCost/api/controllers/UberApiControllerController.js
@@ -0,0 +1,11 @@
+/**
+ * UberApiControllerController
+ *
+ * @description :: Server-side logic for managing Uberapicontrollers
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
