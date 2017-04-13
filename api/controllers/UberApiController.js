435a6bb041fd83c3c7f3f6538270648f00e32f9c
shakti lucifer <shakticse@gmail.com>
2017-04-12 23:02:42 -0400

    Add node wrapper for uber api

--- api/controllers/UberApiController.js
+++ api/controllers/UberApiController.js
@@ -5,23 +5,25 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 var request = require('request');
+var Uber = require('node-uber');
 
 const options = {
-  url: 'https://www.reddit.com/r/funny.json',
+  url: 'https://login.uber.com/oauth/v2/authorize?client_id=XYSz&response_type=code',
   method: 'GET',
   headers: {
     'Accept': 'application/json',
-    'Accept-Charset': 'utf-8',
-    'User-Agent': 'my-reddit-client'
+    'Accept-Charset': 'utf-8'
+    // 'User-Agent': 'my-reddit-client'
   }
 };
+
 module.exports = {
   index: function (req, res) {
 
 
     request(options, function (err, response, body) {
-      let json = JSON.parse(body);
-      console.log(json);
+      // let json = JSON.parse();
+      console.log(body);
     });
     return res.view('homepage', {
       currentDate: (new Date()).toString()
